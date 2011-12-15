ebbdf5162bc4b05276cdf3e0c98504bd0afd32ee
Hao Wei <hao5838@hotmail.com>
2011-12-14 20:23:17 -0500

    changed location of music

--- src/controlClasses/RunCavemanGame.java
+++ src/controlClasses/RunCavemanGame.java
@@ -7,9 +7,9 @@ public class RunCavemanGame
 	public static Music mainSong;
 	public static Music boulderPush;
 	public static Music victorySong;
-	public static String songName = "ggggg.wav";
-	public static String pushingSound = "boulder_push.wav";
-	public static String victorySongFilename = "victory_sound.wav";
+	public static String songName = "music/ggggg.wav";
+	public static String pushingSound = "music/boulder_push.wav";
+	public static String victorySongFilename = "music/victory_sound.wav";
 	
 	/**
 	 * the first method that runs
