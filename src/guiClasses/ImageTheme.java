f3db07aa9dfa7229712275c339c32030fa539554
Ian Dewan <niuren5@gmail.com>
2011-12-18 19:55:33 -0500

    Created the ImageUse enum.

--- src/guiClasses/ImageTheme.java
+++ src/guiClasses/ImageTheme.java
@@ -21,7 +21,6 @@ public class ImageTheme {
 		if (!src.isDirectory())
 			throw new IllegalArgumentException(
 					"Only directories can be passed to ImageTheme()");
-		
 	}
 	
 	public static ImageTheme getThemeByName(String name) {
@@ -35,7 +34,45 @@ public class ImageTheme {
 	}
 	
 	public static ImageTheme getDefaultTheme() {
-		return getThemeByName("default");
+		return getThemeByName("original");
+	}
+	
+	/**
+	 * This enumerated type represents an individual image that appears in a theme.
+	 * @author Ian Dewan
+	 *
+	 */
+	public static enum ImageUse {
+		/** A pushable thing. */
+		BOULDER ("obj1"),
+		/** An un-pushable thing*/
+		WALL ("obj2"),
+		/** The player */
+		PLAYER ("obj3"),
+		/** An area that can be walked over */
+		EMPTY ("obj4"),
+		/** A square you must step on to win */
+		EXIT ("obj5"),
+		/** Swallows up boulders (kills player?) */
+		HOLE ("obj6");
+		/*
+		 * Add new images like this:
+		 * INTERNAL_NAME ("image_file_name")
+		 */
+		
+		private final String file;
+		
+		ImageUse (String file) {
+			this.file = file;
+		}
+		
+		/**
+		 * Return the filename associated with the image type.
+		 * @return The filename, minus extension
+		 */
+		String getFile() {
+			return file;
+		}
 	}
 
 }
